53bc6920d5837f40eb9d9af195c183ac42ad069c
Kamil Różański <kgrozanski@agora.pl>
2020-12-09 17:03:05 +0100

    config added and logger added

--- main.js
+++ main.js
@@ -4,56 +4,56 @@ var path = require('path');
 const chalk = require('chalk');
 
 
-let PORT = 3001;
+let PORT = 3030;
 
 //CONFIGURE////////
-let service = 'Buzz';
-let version = '190';
-let pageType = '99';
+let service = 'Zdrowie';
+let version = '151';
+let pageType = '7';
 ///////////////////
 
 // AWD
-app.get('/mobi', function (req, res) {
+app.get('/mobile', function (req, res) {
     const filePath = `${convertDirname(__dirname)}projects/AWD/modules/deploy/webpack/production/${service}/${version}/pagetype${pageType}/style-mobile-dev.css`;
-    console.log(
-        chalk.bold("MOBILE: ") +
-        chalk.green(filePath + chalk.yellow.bold(" HAS BEEN SERVED"))
-    );
+    logger(req.url.substring(1).toUpperCase(), filePath);
     res.sendFile(path.join(filePath));
 });
 
 app.get('/desktop', function (req, res) {
     const filePath = `${convertDirname(__dirname)}projects/AWD/modules/deploy/webpack/production/${service}/${version}/pagetype${pageType}/style-desk-dev.css`;
-    console.log(
-        chalk.bold("DESKTOP: ") +
-        chalk.green(filePath + chalk.yellow.bold(" HAS BEEN SERVED"))
-    );
+    logger(req.url.substring(1).toUpperCase(), filePath);
     res.sendFile(path.join(filePath));
 });
+
 
 //BLOGCMS
 app.get('/blogcms_js', function (req, res) {
     const filePath = `${convertDirname(__dirname)}projects/C2S/blogcms/cms-uom-web/src/main/webapp/production/sst/sst-dev.js`;
-    console.log(
-        getFullHour() + ' ' +
-        chalk.bold("blogcms_js: ") +
-        chalk.green(filePath + chalk.yellow.bold(" HAS BEEN SERVED"))
-    );
+    logger(req.url.substring(1).toUpperCase(), filePath);
     res.sendFile(path.join(filePath));
 });
+
 app.get('/blogcms_css', function (req, res) {
     const filePath = `${convertDirname(__dirname)}projects/C2S/blogcms/cms-uom-web/src/main/webapp/themes/sst/content.css`;
-    console.log(
-        getFullHour() + ' ' +
-        chalk.bold("blogcms_css: ") +
-        chalk.green(filePath + chalk.yellow.bold(" HAS BEEN SERVED"))
-    );
+    logger(req.url.substring(1).toUpperCase(), filePath);
     res.sendFile(path.join(filePath));
 });
 
 app.listen(PORT, () => {
-    console.log('༼ つ ◕_◕ ༽つ ' + chalk.yellow.bold.underline('FILE SERVER HAS IS LISTENING AT PORT:') + ' ' + chalk.green.bold(PORT) )
-})
+    let log = ` 
+${chalk.yellow('╔═══════════════════════════════════════════════════╗')}
+${chalk.yellow('║')}  ༼ つ ◕_◕ ༽つ ${chalk.bold('FILE SERVER STARTED AT PORT:')} ${chalk.green(PORT)}   ${chalk.yellow('║')}
+${chalk.yellow('╚═══════════════════════════════════════════════════╝')}`;
+    console.log(log);
+});
+
+function logger(prefix, text) {
+    console.log(
+        getFullHour() + ' ' +
+        chalk.bold(prefix) + ':',
+        chalk.green(text + chalk.yellow.bold(" HAS BEEN SERVED"))
+    );
+}
 
 function convertDirname(path) {
     const sliced = path.split("\\");
